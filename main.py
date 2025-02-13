4afb4b0badd485f72ccad143c4fc525bedf01807
Vadim Chaykin <snusmumrike2@gmail.com>
2025-02-13 12:45:56 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+from src.gui.screens import *
+
+class MultimediaApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name="main"))
+        sm.add_widget(ImageAnalyzerScreen(name="image"))
+        sm.add_widget(VideoConcatenatorScreen(name="video"))
+        sm.add_widget(AudioVisualizerScreen(name="audio"))
+        return sm
+
+if __name__ == "__main__":
+    Builder.load_file("src/gui/gui.kv")
+    MultimediaApp().run()
---
+++ src/gui/screens.py
@@ -0,0 +1,13 @@
+from kivy.uix.screenmanager import Screen
+
+class MainScreen(Screen):
+    pass
+
+class ImageAnalyzerScreen(Screen):
+    pass
+
+class VideoConcatenatorScreen(Screen):
+    pass
+
+class AudioVisualizerScreen(Screen):
+    pass
