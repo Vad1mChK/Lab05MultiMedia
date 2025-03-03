24157347c44b002b44242240c2d6a7f36499ac4f
Vadim Chaykin <snusmumrike2@gmail.com>
2025-03-03 12:55:51 +0300

    added cancellation and improved error messages

--- apps/video_downloader.py
+++ apps/video_downloader.py
@@ -13,9 +13,14 @@ from pydub import AudioSegment  # For MP3 conversion
 from src.util.string_utils import distill_filename
 
 
+class CancelledException(Exception):
+    pass
+
+
 class DownloadWorker(QThread):
     progress_update = Signal(int)
     finished = Signal()
+    success = Signal()
     error = Signal(str)
     video_name_change = Signal(str)
 
@@ -25,6 +30,7 @@ class DownloadWorker(QThread):
         self.save_dir = save_dir
         self.is_playlist = is_playlist
         self.mode = mode
+        self.is_cancelled = False
 
     def run(self):
         try:
@@ -40,15 +46,22 @@ class DownloadWorker(QThread):
                         self.download_single(self.link, self.mode)
             else:
                 self.download_single(self.link, self.mode)
-            self.finished.emit()
+            self.success.emit()
         except Exception as e:
             self.error.emit(str(e))
+            return
+        # finally:
+        #     self.finished.emit()
 
     def on_progress(self, stream, chunk, bytes_remaining):
         total_size = stream.filesize
         bytes_downloaded = total_size - bytes_remaining
         percentage = int(bytes_downloaded / total_size * 100)
         self.progress_update.emit(percentage)
+
+    def cancel(self):
+        self.error.emit('Download cancelled by user.')
+        self.terminate() # This approach is highly discouraged as it stops immediately without cleanup
 
     def download_single(self, link: str, mode: str):
         yt = YouTube(link, on_progress_callback=self.on_progress)
@@ -59,17 +72,20 @@ class DownloadWorker(QThread):
                 self.convert_to_mp3(file_path)
 
         yt.register_on_complete_callback(on_complete_callback)
+        yt.register_on_progress_callback(self.on_progress)
 
         self.video_name_change.emit(yt.title)
 
         if mode == "audio":
             stream = yt.streams.filter(only_audio=True).first()
-            if stream:
-                stream.download(output_path=self.save_dir)
+            if not stream:
+                raise ValueError("Audio stream not available.")
+            stream.download(output_path=self.save_dir)
         else:
             stream = yt.streams.get_highest_resolution()
-            if stream:
-                stream.download(output_path=self.save_dir)
+            if not stream:
+                raise ValueError("Video stream not available.")
+            stream.download(output_path=self.save_dir)
 
         self.progress_update.emit(100)
         print("Single video download complete.")
@@ -154,6 +170,8 @@ class VideoDownloader(QMainWindow):
         self.download_progress: QProgressBar = self.findChild(QProgressBar, "downloadProgressBar")
         self.video_name_label: QLabel = self.findChild(QLabel, "videoNameLabel")
 
+        self.downloading = False
+
         self.save_dir = None
         self.link = None
         self.download_progress.setValue(0)
@@ -161,7 +179,7 @@ class VideoDownloader(QMainWindow):
         # Connect signals.
         self.link_input.textChanged.connect(lambda text: self.on_change_link(text))
         self.choose_dir_button.clicked.connect(self.on_choose_directory)
-        self.download_button.clicked.connect(self.on_download)
+        self.download_button.clicked.connect(self.on_click_download_button)
 
         self.worker = None
 
@@ -183,6 +201,17 @@ class VideoDownloader(QMainWindow):
         self.download_button.setEnabled(is_enabled)
         self.download_progress.setEnabled(is_enabled)
 
+    def on_click_download_button(self):
+        self.downloading = not self.downloading
+        self.update_download_button_text()
+        if self.downloading:
+            self.on_download()
+        else:
+            self.on_cancel_download()
+
+    def update_download_button_text(self):
+        self.download_button.setText('Cancel' if self.downloading else 'Download')
+
     def update_video_name_label(self, text: str, is_error: bool = False, is_video_name: bool = False):
         self.video_name_label.setText(text)
         font = self.video_name_label.font()
@@ -190,15 +219,25 @@ class VideoDownloader(QMainWindow):
         font.setItalic(is_error)
         self.video_name_label.setFont(font)
 
-    def on_download_finished(self):
-        print("Download finished.")
+    def on_download_success(self):
+        print("Download succeeded.")
         self.update_video_name_label('Download completed.')
-        self.toggle_download_if_ready()
 
     def on_download_error(self, err):
         print("Download error:", err)
-        self.update_video_name_label(f'Download error: {err}')
+        self.update_video_name_label(f'Download error: {err}', is_error=True)
+
+    def on_download_finished(self):
+        print("Download finished")
         self.toggle_download_if_ready()
+        self.downloading = False
+        self.update_download_button_text()
+
+    def on_cancel_download(self):
+        print("Cancelling download")
+        if self.worker is None:
+            raise ValueError("Worker not initialized.")
+        self.worker.cancel()
 
     def on_download(self):
         link = self.link
@@ -209,7 +248,7 @@ class VideoDownloader(QMainWindow):
             print("No save directory chosen.")
             return
 
-        self.download_button.setEnabled(False)
+        # self.download_button.setEnabled(False)
         self.download_progress.setValue(0)
         is_playlist = self.download_playlist_check.isChecked()
         mode = "audio" if self.radio_audio.isChecked() else "video"
@@ -218,8 +257,10 @@ class VideoDownloader(QMainWindow):
         self.worker = DownloadWorker(link, self.save_dir, is_playlist, mode)
         self.worker.progress_update.connect(self.download_progress.setValue)
         self.worker.error.connect(self.on_download_error)
+        self.worker.success.connect(self.on_download_success)
         self.worker.finished.connect(self.on_download_finished)
-        self.worker.video_name_change.connect(lambda video_name: self.update_video_name_label(video_name, is_video_name=True))
+        self.worker.video_name_change.connect(
+            lambda video_name: self.update_video_name_label(video_name, is_video_name=True))
         self.worker.start()
 
 
